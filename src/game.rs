14233e255fb6d898971a5d68eece3a178cc7bcf0
Joshua Morton <joshua.morton13@gmail.com>
2018-01-06 21:02:09 -0500

    Made the trait a smidge more likeable.

--- src/game.rs
+++ src/game.rs
@@ -9,15 +9,16 @@ pub enum Player {
 ///
 /// A Game need only be able to provide:
 ///  - An accessor method for the current player
-///  - An accessor method for the game state
+///  - A getter and setter for the game state
 ///  - A list of valid moves for a player, from a given state
-///  - A way to see the state after a move completes
+///  - A way to see the impact of a move on the state
 ///  - A check to see if a player won the game
 pub trait Game<State, Move> {
     fn get_player(&self) -> Player;
     fn get_state(&self) -> State;
+    fn set_state(&mut self, state: State);
     fn next_move(&self, player: Player) -> Box<Vec<Move>>;
-    fn evaluate(&self, action: Move) -> State;
-    fn won(&self, player: Player) -> bool;
+    fn evaluate(state: State, action: Move) -> State;
+    fn won(state: State, player: Player) -> bool;
 }
 
