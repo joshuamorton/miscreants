94ec221129227036d4371b0e588e4941ce9e4428
Joshua Morton <joshua.morton13@gmail.com>
2018-01-08 03:42:11 -0500

    Added a way to sample a beta distribuion.

--- src/mcts.rs
+++ src/mcts.rs
@@ -1,14 +1,27 @@
+extern crate probability;
+
 use std::hash::Hash;
 use std::cmp::Eq;
 use std::collections::HashMap;
 use std::marker::PhantomData;
+use mcts::probability::distribution::Beta;
+use mcts::probability::distribution::Sample;
+use mcts::probability::prelude::source;
+use mcts::probability::prelude::source::Source;
+use mcts::probability::prelude::source::Default;
 
 use game;
+
+fn beta_sample(alpha: f64, beta: f64, src: &mut Default) -> f64 {
+    let b = Beta::new(alpha, beta, 0.0, 1.0);
+    return b.sample(src);
+}
 
 pub struct MonteCarloAgent<State, Move: Copy + Hash + Eq, G: game::Game<State, Move>> {
     game: G,
     playouts: MCTSNode<Move>,
     phantom: PhantomData<State>,
+    source: Default,
 }
 
 impl<State, Move: Copy + Hash + Eq, G: game::Game<State, Move>> MonteCarloAgent<State, Move, G> {
@@ -17,6 +30,7 @@ impl<State, Move: Copy + Hash + Eq, G: game::Game<State, Move>> MonteCarloAgent<
             game: G::new(),
             playouts: MCTSNode::new(),
             phantom: PhantomData,
+            source: source::default(),
         }
     }
 }
@@ -40,4 +54,9 @@ impl<Move: Hash + Eq> MCTSNode<Move> {
 struct Rollout<State, Move: Copy> {
     moves: Vec<Move>,
     state: State,
+}
+
+
+#[cfg(test)]
+mod tests {
 }
